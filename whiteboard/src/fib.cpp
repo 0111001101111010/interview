83dea21737f0b892c5b93e9fd462ce20e2b9a67c
stanzheng <dev.stanzheng@gmail.com>
2014-03-07 00:36:13 -0500

    never do fib this way

--- whiteboard/src/fib.cpp
+++ whiteboard/src/fib.cpp
@@ -6,11 +6,13 @@ Notes: with recursion: Not Ideal
 #include <iostream>
 
 int fib (int);
+long long longFib(int);
 
 int main(int argc, char* argv[]){
-
+  int num = atoi(argv[1]);
 //std::cout<< argv[0] << " " << argv[1];
-  std::cout << fib(6);
+  std::cout << fib(num) << std::endl;
+  std::cout << longFib(num) << std::endl;
 return 0;
 }
 
