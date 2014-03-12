3bf826631ca516142c72f3759384d46d24827908
stanzheng <dev.stanzheng@gmail.com>
2014-03-11 23:48:17 -0400

    im sad

--- whiteboard/src/patrick.cpp
+++ whiteboard/src/patrick.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
+
 using namespace std;
+
 struct Node{
-int data;
-Node* next;
+	int data;
+	Node* next;
 };
+
 int main(int argc, char* argv[]){
 
-Node* head =NULL;
+Node* head=NULL;
 Node* temp=NULL;
-temp = new Node;
 
+	temp = new Node;
 //create list 0-2
 for (int i = 0; i < 3; i++){
+	//make it a node box
 	temp ->data = i;
-
-	temp->next=NULL;
-	head = temp;
+	temp->next= head;
+	head=temp;
 }
 Node* current = head;
 
