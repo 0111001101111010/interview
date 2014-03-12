29c63300535fa48b70645e7ca7b6847b74fc7c92
stanzheng <dev.stanzheng@gmail.com>
2014-03-12 00:03:22 -0400

    append to end

--- whiteboard/src/patrick.cpp
+++ whiteboard/src/patrick.cpp
@@ -14,16 +14,36 @@ Node* temp=NULL;
 
 
 //create list 0-2
+//adding to the head
 for (int i = 0; i < 3; i++){
 	//make it a node box
 	temp = new Node;
 	temp ->data = i;
+	//add to head
 	temp->next= head;
 	head=temp;
 }
+
+//adding to the tail
+for (int i = 3; i < 6; i++){
+	//make it a node box
+	temp = new Node;
+	temp ->data = i;
+	temp ->next =NULL;
+	//add to tail
+	Node* current = head;
+	while(current->next!=NULL){
+		current=current->next;
+	}
+	current->next=temp;
+}
+
+//removing a value
 Node* current = head;
 
+
 //printer
+Node* current = head;
 while(current!=NULL){
 	cout << current->data;
 	current=current->next;
