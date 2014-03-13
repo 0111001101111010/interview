7b1bf29d21615532dd21d4480c7ac65c48fdc438
stanzheng <dev.stanzheng@gmail.com>
2014-03-13 13:01:03 -0400

    pointer references

--- whiteboard/src/prog5.cpp
+++ whiteboard/src/prog5.cpp
@@ -1,38 +1,29 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-{
-public:
-Stack() : head( ~Stack();
-void push( void void *pop();
-protected:
-class Element { public:
-NULL ) {}; *data );
-Element( Element *n, void *d ): next(n), data( d ) {} Element *getNext() const { return next; }
-void *value() const { return data; }
-private:
-Element *next; void *data;
-};
-Element *head; };
-Stack::~Stack() { while( head ){
-Element *next = head->getNext(); delete head;
-head = next;
-} }
-
-void Stack::push( void *data ){
-//Allocation error will throw exception Element *element = new Element(head,data); head = element;
-}
-void *Stack::pop() {
-Element *popElement = head; void *data;
-/* Assume StackError exception class is defined elsewhere */ if( head == NULL )
-throw StackError( E_EMPTY );
-data = head->value(); head = head->getNext(); delete popElement; return data;
-}
-
 int main(int argc, char* argv[]){
 
-Element stack;
+	int *ip,*iq;
+	int i=5, j=0;
+	iq = &i;
+	ip = &i;
+	cout << ip << "\n";
+	printf("%d\n", *ip);
+	i=4;
+	iq=ip;
+	cout << ip << "\n";
+	printf("%d\n", *ip);
 
+	ip=&j;
+	cout << ip << "\n";
+	printf("%d\n", *ip);
+	j=5;
+	ip=&j;
+	cout << ip << "\n";
+	printf("%d\n", *ip);
+
+//comparisons
 return 0;
 }
