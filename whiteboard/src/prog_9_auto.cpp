0b58a86038bc4ae27fa825382a228cdaeb622f79
stanzheng <dev.stanzheng@gmail.com>
2014-03-13 22:19:36 -0400

    auto is amazing

---
+++ whiteboard/src/prog_9_auto.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+struct A {
+  A() { std::cout << "A"; }
+  A(const A &a) { std::cout << "B"; }
+  virtual void f() { std::cout << "C"; }
+};
+
+int main() {
+  A a[2];
+  for (auto x : a) {
+    x.f();
+  }
+
+std::cout <<"\n";
+
+//Vectors
+std::vector<int> list;
+list.push_back(1);
+list.push_back(2);
+
+  for (auto x : list){
+  	std::cout <<x;
+  }
+//Normal Array
+std::cout <<"\n";
+  int array[5];
+  for (auto x : array){
+  	std::cout <<x;
+  }
+}
