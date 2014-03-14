e97b195c5a80054adf63698c336f9bd741632465
stanzheng <dev.stanzheng@gmail.com>
2014-03-14 14:09:30 -0400

    testing array iterators

--- whiteboard/src/Test.java
+++ whiteboard/src/Test.java
@@ -1,10 +1,21 @@
 //java test stub
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Test{
+	//private ArrayList<Card> mlcCards = new ArrayList<Card>();
+    private ArrayList<String> mlsText = new ArrayList<String>(Arrays.asList("9875-4321", "1234-5678"));
 
 	public static void main(String[] args){
 
 	System.out.print("HI");
+	}
+	public int print(){
 
+	for (int i = 0; i < mlsText.size(); i++)
+        {
+        System.out.print(mlsText[i]);
+        }
 	}
 }
