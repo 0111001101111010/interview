7fd5ebe6cd4e6e9c5927f2f41e93a9ec0f4f3bab
stanzheng <dev.stanzheng@gmail.com>
2014-03-17 12:27:45 -0400

    finalized

--- whiteboard/src/prog18-string.cpp
+++ whiteboard/src/prog18-string.cpp
@@ -5,30 +5,20 @@
 
 using std::cout;
 int main(){
+	//start
+	std::string word = "boats";
+	cout << word << "\n";
+	char temp;
+	//reverse in place!
+	for (int i=0; i < (word.size()/2);i++){
+	 	temp = word[i];
+		word[i]=word[(word.size()-1-i)];
+		word[word.size()-1-i]=temp;
+	 }
+	cout << word << "\n";
 
-//start
-std::string word = "boats";
-cout <<word <<"\n";
-char temp;
-/*
-temp=word[0];
-word[0]=word[(word.size()-1)];
-word[word.size()-1]=temp;
-cout <<word <<"\n";
-
-//second go around
-temp=word[1];
-word[1]=word[(word.size()-1-1)];
-word[word.size()-1-1]=temp;
-cout <<word <<"\n";*/
-
-//reverse in place!
-for (int i=0; i < (word.size()/2);i++){
- 	temp = word[i];
-	word[i]=word[(word.size()-1-i)];
-	word[word.size()-1-i]=temp;
- //cout <<i;
- }
-cout << word;
-return 0;
+	//algorithm reverse reverse
+	std::reverse(word.begin(),word.end());
+	cout <<word << "\n";
+	return 0;
 };
