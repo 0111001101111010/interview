e3f45145954ea703acf46c6ce88319d49f96b9e5
stanzheng <dev.stanzheng@gmail.com>
2014-03-18 12:28:01 -0400

    adding more data structures

--- cracking/hashmap.cpp
+++ cracking/hashmap.cpp
@@ -1,10 +1,14 @@
+/* Quick Datastructure review
+* stanley zheng
+*/
+
 #include <cstdlib>
 #include <iostream>
 #include <stack>
 #include <queue>
 int main(int argc, char const *argv[])
 {
-
+//stack LIFO
 	std::stack<int> a;
 	for (int i=0; i<5; i++)
 		a.push(i);
@@ -12,13 +16,22 @@ int main(int argc, char const *argv[])
 	a.pop();
 	std::cout << a.top();
 
-
+//queue FIFO
+	//also Deque and enque
 	std::queue<int> b;
 	for (int i=0; i<5; i++)
 		b.push(i);
 	std::cout<<"\n";
 	b.pop();
 	std::cout << b.front();
+//hashmap
+	std::map<int> c;
+for (int i=0; i<5; i++)
+		b.push(i);
+	std::cout<<"\n";
+	b.pop();
+	std::cout << b.front();
+//pair
 	/* code */
 	return 0;
 }
