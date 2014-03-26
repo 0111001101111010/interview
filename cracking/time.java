3d95cc3b71cbfcf0ae52e7be7d8ccc65b9bfeeba
stanzheng <dev.stanzheng@gmail.com>
2014-03-25 23:06:24 -0400

    not working

--- cracking/time.java
+++ cracking/time.java
@@ -3,11 +3,14 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 class time extends TimerTask {
+    public static void main(String[] args) {
+	run();		
+    }
     public void run() {
        System.out.println("Hello World!");
     }
  // And From your main() method or any other method
  Timer timer = new Timer();
- timer.schedule(new SayHello(), 0, 5000);
+ timer.schedule(new run(), 0, 5000);
 
  }
